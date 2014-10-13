5e184520b3e9f66c9158e8cd303b21cbd390fdda
Jason Greaves <jxg4000@rit.edu>
2014-10-13 11:11:48 -0400

    Assets folder has seperate folders to help with organization. Player object exists and can move (Not finalized). Collectable exists but is just a small cube.

---
+++ unity-project/Assets/Scripts/PlayerInput.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var speed : int;
+
+function FixedUpdate()
+{
+	var h : float = Input.GetAxis("Horizontal");
+	var v : float = Input.GetAxis("Vertical");
+	
+	var movement : Vector3 = new Vector3 (v, 0.0f, -h);
+	var pos : Vector3 = transform.position; 
+	
+	pos = pos + movement * speed * Time.deltaTime;
+	
+	transform.position = pos;
+}
