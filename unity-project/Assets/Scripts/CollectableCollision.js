20d5650200056779503022ac8c699c42eace4772
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2014-11-04 15:08:10 -0500

    changed game controls

--- unity-project/Assets/Scripts/CollectableCollision.js
+++ unity-project/Assets/Scripts/CollectableCollision.js
@@ -3,5 +3,6 @@
 function OnCollisionEnter(collision:Collision){
 	if(collision.gameObject.tag == "Player"){
     	Destroy(this.gameObject);
+    	collision.gameObject.SendMessage("HitCollectable");
     }
 }
--- unity-project/Assets/Scripts/PlayerInput.js
+++ unity-project/Assets/Scripts/PlayerInput.js
@@ -8,10 +8,12 @@ public var rotSpeed : int;
 function FixedUpdate()
 {
 	//get value for when the user presses a horizontal key
-	var h : float = Input.GetAxis("Horizontal");
+	//var h : float = Input.GetAxis("Horizontal");
+	var x : float = Input.GetAxis("Mouse X");
+	var y : float = Input.GetAxis("Mouse Y")*(-0.5);
 	
 	//create a vector3 value for when our ship rotates
-	var rotate : Vector3 = new Vector3(0.0f, h, 0.0f);
+	var rotate : Vector3 = new Vector3(y, x, 0.0f);
 	
 	//this is the current rotation of the ship
 	var rot : Vector3 = transform.rotation.eulerAngles;
@@ -25,14 +27,23 @@ function FixedUpdate()
 	//if the forward button is pressed, move forward at the desired speed.
 	if(Input.GetButton("Forward"))
 	{
-		transform.position += transform.up * speed * Time.deltaTime;
+		transform.position += transform.forward * speed * Time.deltaTime;
 	}
 	//back button pressed, move backward at the desired speed.
 	if(Input.GetButton("Back"))
 	{
-		transform.position -= transform.up * speed * Time.deltaTime;
+		transform.position -= transform.forward * speed * Time.deltaTime;
 	}
 	
 	//Update the ships rotation 
 	transform.rotation.eulerAngles = rot + rotate * rotSpeed * Time.deltaTime;
+}
+
+function HitCollectable(){
+	Debug.Log("Hit collectable");
+}
+
+function HitFluid(){
+	speed = .1;
+	Debug.Log("Hit fluid");
 }
