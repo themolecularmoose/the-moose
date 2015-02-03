0528daa248a17923e6d2677048e61b4f8a42a8a8
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2015-02-03 14:03:38 -0500

    Converted player collisions to c#.

---
+++ unity-project/Assets/Scripts/PlayerCollison.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerCollison : MonoBehaviour
+{
+	public GameController gameController;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rigidbody.freezeRotation = true;
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if(gameControllerObject != null){
+			gameController = gameControllerObject.GetComponent("GameController") as GameController;
+		}else{
+			Debug.Log("Can't find 'GameController' script!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void FixedUpdate () {
+		if(Input.GetButton("Tractor Beam"))
+		{
+			gameController.beamState(true);
+		}
+		else
+		{
+			gameController.beamState(false);
+		}
+	}
+
+	void HitFluid(){
+		Debug.Log("Hit fluid");
+	}
+	
+	void HitObject()
+	{
+		Debug.Log("Hit base!");
+		wait ();
+		Debug.Log("Good to go");
+	}
+
+	IEnumerator wait()
+	{	
+		yield return new WaitForSeconds (1);
+	}
+}
+
