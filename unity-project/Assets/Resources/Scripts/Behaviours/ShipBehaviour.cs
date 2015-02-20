ef1bfa2cec591f619e892e1b50e7980a9a95714c
CptTime <tls8963@rit.edu>
2015-02-20 14:43:17 -0500

    +Ship Behaviour

--- unity-project/Assets/Resources/Scripts/Behaviours/ShipBehaviour.cs
+++ unity-project/Assets/Resources/Scripts/Behaviours/ShipBehaviour.cs
@@ -1,31 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
+using System.Threading;
 
 public class ShipBehaviour : MonoBehaviour {
-	private int beamEnergy;
-	private int collected;
-	private int waterCount;
-	private int methaneCount;
-	public ArrayList collected_collectables;
-	private float health;
 	private bool tractorBeam;
+	private int beamEnergy;
+	private float health;
+	// Convience var for modifing damage upwards
+	private float damageScalar = 1;
+	// List of objects that can cause damage
+	private string[] damagers = {"Wall", "Obsticle"};
+	// Ensures order of damage taken
+	private static Mutex _m;
+	private GameController gameController;
 
-	// Use this for initialization
-	void Start () {
-		collected_collectables = new ArrayList();
-		beamEnergy = 100;
-		waterCount = 0;
-		methaneCount = 0;
-		collected = 0;
-		tractorBeam = false;
-		waterCount = 0;
-		methaneCount = 0;
-		health = 100;
+	public int BeamEnergy
+	{
+		get{ return beamEnergy;}
+		set{ beamEnergy = value;}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public void beamState(bool state)
+	{
+		if(beamEnergy <= 0)
+		{
+			tractorBeam = false;
+		}
+		else
+		{
+			tractorBeam = state;
+		}
+		
+	}
 	
+	float CalcDamage(Collision hit) {
+		float hitMagnitude = hit.relativeVelocity.magnitude;
+		float pointsOfContact = hit.contacts.Length;
+		float mass = 0;
+		if (hit.rigidbody != null) {
+			mass = hit.rigidbody.mass;
+		} else {
+			mass = 1; // TODO: Brian S -> Add custom variables to game objects to avoid use of rigidbody on static objects
+		}
+		float force = mass * hitMagnitude;
+		float forceSpread = 1; // Default to one damamge for any collision
+		if (force > pointsOfContact && pointsOfContact != 0) {
+			// Naively spread damamge over points of collision
+			forceSpread = Mathf.FloorToInt (force) / pointsOfContact;
+		}
+		return forceSpread * damageScalar;
+	}
+	
+	void CheckDeath(){
+		if(gameController.GetHealth() <= 0) {
+			gameObject.SendMessage("OnDeath");
+		}
+	}
+	
+	public void DecreaseHealth(float damage) {
+		health -= damage;
 	}
 
 	/// <summary>
@@ -40,17 +74,41 @@ public class ShipBehaviour : MonoBehaviour {
 		{
 			beamState(false);
 		}
+		CheckDeath ();
+	}
+
+	public float Health
+	{
+		get{ return health;}
+		set{ health = value;}
 	}
 	
-	public void beamState(bool state)
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision)
 	{
-		if(beamEnergy <= 0)
-		{
-			tractorBeam = false;
+		string collidedWithTag = collision.gameObject.tag;
+		
+		// If game object collided with is not in damagers list -> exit
+		if (!damagers.Contains (collidedWithTag)) {
+			return;
 		}
-		else
-		{
-			tractorBeam = state;
-		}
+
+		_m.WaitOne();
+		float damage = CalcDamage (collision);
+		gameObject.SendMessage ("OnDamage", damage);
+		_m.ReleaseMutex ();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		beamEnergy = 100;
+		tractorBeam = false;
+		health = 100;
+	}
+
+	public bool TractorBeam
+	{
+		get{ return tractorBeam;}
+		set{ tractorBeam = value;}
 	}
 }
