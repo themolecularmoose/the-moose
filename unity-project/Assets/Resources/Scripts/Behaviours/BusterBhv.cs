9ae4a1f559fb0919b7daf8f7bd1935f864a70732
Tim Steen <tls8963@RIT.EDU>
2015-02-23 13:57:11 -0500

    +Cluster Prefabs +Cluster Behaviour +Buster Prefabs +Buster Behaviour +Fixed Methane Molecule

---
+++ unity-project/Assets/Resources/Scripts/Behaviours/BusterBhv.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BusterBhv : MonoBehaviour {
+	public float m_speed;
+	public float m_power;
+	public float m_lift;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * m_speed;
+	}
+
+	void OnTriggerEnter(Collider a_other)
+	{
+		a_other.gameObject.SendMessageUpwards("onBusterHit", this, SendMessageOptions.DontRequireReceiver);
+	}
+}
---
+++ unity-project/Assets/Resources/Scripts/Behaviours/ClusterBhv.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClusterBhv : MonoBehaviour {
+	bool m_broken;
+
+	// Use this for initialization
+	void Start () {
+		Transform child;
+		//disable all colectable scripts
+		for(int i = 0; i < transform.childCount; ++i)
+		{
+			child = transform.GetChild(i);
+			MonoBehaviour[] scripts = child.GetComponents<MonoBehaviour>();
+			for(int s = 0; s <scripts.Length; ++s)
+			{
+				scripts[s].enabled = false;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	void Explode(float a_power, float a_lift){
+		Explode(a_power, a_lift, transform.position);
+	}
+	void Explode(float a_power, float a_lift, Vector3 a_center){
+		m_broken = true;
+		Transform child;
+		//disable all colectable scripts
+		for(int i = 0; i < transform.childCount; ++i)
+		{
+			child = transform.GetChild(i);
+			MonoBehaviour[] scripts = child.GetComponents<MonoBehaviour>();
+			//enable scripts
+			for(int s = 0; s <scripts.Length; ++s)
+			{
+				scripts[s].enabled = true;
+			}
+			//break child off
+			child.SetParent(null);
+			i--;
+			//add explosive force
+			if(child.rigidbody != null)
+			{
+				child.rigidbody.constraints = RigidbodyConstraints.None;
+				child.rigidbody.AddExplosionForce(a_power, a_center, 0, a_lift);
+			}
+		}
+	}
+
+	void onBusterHit(object a_sender)
+	{
+		if(!m_broken)
+		{
+			Debug.Log("Cluster Compromised");
+			BusterBhv buster = (BusterBhv)a_sender;
+			Explode(buster.m_power, buster.m_lift, buster.gameObject.transform.position);
+			Destroy(buster.gameObject);
+		}
+	}
+}
