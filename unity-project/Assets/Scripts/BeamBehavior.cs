6730ff10aca4e948bc962e9a7bd1eae7299df3d3
Jason Greaves <jxg4000@rit.edu>
2015-03-02 23:16:50 -0500

    Forgot to commit the script because I'm good at git

---
+++ unity-project/Assets/Scripts/BeamBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamBehavior : MonoBehaviour {
+
+	private bool stick = false;
+	private bool jointed = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Tractor Beam"))
+		{
+			stick = true;
+		}
+		else
+		{
+			stick = false;
+			DeleteJoint();
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if(stick && !jointed)
+		{
+			CreateJoint(other.rigidbody);
+		}
+	}
+
+	private void CreateJoint(Rigidbody rb)
+	{
+		FixedJoint joint = gameObject.AddComponent<FixedJoint> ();
+		joint.connectedBody = rb;
+		jointed = true;
+	}
+
+	private void DeleteJoint()
+	{
+		FixedJoint joint = gameObject.GetComponent<FixedJoint> ();
+		Destroy (joint);
+		jointed = false;
+	}
+}
