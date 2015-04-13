bb4aab2fb35dbb4153e816ad4ea10864900c92be
Jason Greaves <jxg4000@rit.edu>
2015-04-12 22:08:03 -0400

    Redid my change with the latest master so that things wouldn't be messed up.

---
+++ unity-project/Assets/Scripts/Audio/ShipAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class ShipAudio : MonoBehaviour {
+	
+	public AudioSource damage;
+	public AudioSource boost;
+	
+	public void PlayDamage()
+	{
+		damage.Play ();
+	}
+	
+	public void PlayBoost()
+	{
+		boost.Play();
+	}
+}
