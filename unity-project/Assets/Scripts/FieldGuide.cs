48a0d74b3c61c6f8e8a8fcb89bd629ec29347619
Gabrielle Bennett <gmb9280@rit.edu>
2015-04-20 11:57:12 -0400

    Field guide toggles on and off with 'p' key for now. Working on getting molecule information integrated into the guide.

---
+++ unity-project/Assets/Scripts/FieldGuide.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldGuide : MonoBehaviour {
+
+	public struct Molecule
+	{
+		public string name; // eg. "Hydrogen Dioxide"
+		public string flavorText; // eg. "This molecule is absolutely necessary for life. It is composed of two oxygens and a hydrogen."
+		public int numCollected; // eg. "0"
+
+		public Molecule(string name, string flavor, int numCollected)
+		{
+			this.name = name; 
+			this.flavorText = flavor; 
+			this.numCollected = numCollected;
+		}
+	}
+
+	public GameObject Canvas; 
+	public Text moleculeName; 
+
+	public List<Molecule> moleculeInfo;
+	public bool guideUp = false; 
+	// Use this for initialization
+	void Start () {
+		Canvas.gameObject.SetActive (false);
+		moleculeInfo = new List<Molecule> ();
+		moleculeName.text = "Hydrogen Dioxide";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("p")) {
+		if(guideUp == true)
+			{
+				Canvas.gameObject.SetActive (false); 
+				Screen.showCursor = false; 
+				Screen.lockCursor = true;
+				guideUp = false;
+			}
+			else{
+				Canvas.gameObject.SetActive (true); 
+				Screen.showCursor = true;
+				Screen.lockCursor = false; 
+				guideUp = true;
+			}
+		}
+	}
+}
