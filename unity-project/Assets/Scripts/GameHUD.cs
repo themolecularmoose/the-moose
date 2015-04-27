11624d7051d316e4293e3288e2c3239db2fb3a72
Brian <bzh8205@rit.edu>
2015-04-27 02:32:26 -0400

    mouse sensitivity is so sexy
    minor issues

--- unity-project/Assets/Scripts/GameHUD.cs
+++ unity-project/Assets/Scripts/GameHUD.cs
@@ -11,6 +11,9 @@ public class GameHUD : MonoBehaviour {
 	public LevelManager level;
 	public LevelLoader loader;
 	public EventPublisher eventPublisher;
+	private MouseLook xaxis;
+	private MouseLook yaxis;
+	private float sens = 15.0f;
 
 	public string[] collectableMolecules = {"Water", "Methane"};
 	public Dictionary<string, Texture> moleculeTextures;
@@ -48,6 +51,8 @@ public class GameHUD : MonoBehaviour {
 		} else { 
 			Debug.Log ("No level game object in scene: " + Application.loadedLevelName);
 		}
+		xaxis = GameObject.Find ("Player").GetComponent<MouseLook> ();
+		yaxis = GameObject.Find ("Attachments").GetComponent<MouseLook> ();
 	}
 	
 	void Update () {
@@ -67,31 +72,54 @@ public class GameHUD : MonoBehaviour {
 			}
 		}
 		if (menupaused) {
-			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), box);
-			GUIStyle topCenterStyle = GUI.skin.GetStyle("Label");
-			topCenterStyle.alignment = TextAnchor.UpperCenter;
-			topCenterStyle.fontSize = 48;
-			topCenterStyle.font = hudFont;
-			topCenterStyle.normal.textColor = Color.yellow;
-			//top center
-			GUI.Label (new Rect (Screen.width / 2 - 100, 25, 200, 100), "PAUSED", topCenterStyle);
-			GUIStyle bottomCenterStyle = GUI.skin.GetStyle("Label");
-			GUIContent content = new GUIContent();
-			bottomCenterStyle.alignment = TextAnchor.MiddleCenter;
-			bottomCenterStyle.fontSize = 48;
-			bottomCenterStyle.font = hudFont;
-			bottomCenterStyle.normal.textColor = Color.white;
-			bottomCenterStyle.hover.textColor = Color.yellow;
-			if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 3 * 2 - 50, 300, 100), "EXIT GAME", bottomCenterStyle)){
-				if( loader != null ){
-					eventPublisher.publish ( new PauseEvent(true) );
-					eventPublisher.publish ( new ShowMouseEvent(true) );
-					loader.LoadLevel("start_menu");
-				} else {
-					Debug.Log ("No level loader found in scene: " + Application.loadedLevelName);
-				}
+			drawPauseMenu();
+		}
+	}
+
+	private void drawPauseMenu(){
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), box);
+		GUIStyle topCenterStyle = GUI.skin.GetStyle("Label");
+		topCenterStyle.alignment = TextAnchor.UpperCenter;
+		topCenterStyle.fontSize = 48;
+		topCenterStyle.font = hudFont;
+		topCenterStyle.normal.textColor = Color.yellow;
+		//top center
+		GUI.Label (new Rect (Screen.width / 2 - 100, 25, 200, 100), "PAUSED", topCenterStyle);
+		GUIStyle bottomCenterStyle = GUI.skin.GetStyle("Label");
+		bottomCenterStyle.alignment = TextAnchor.MiddleCenter;
+		bottomCenterStyle.fontSize = 36;
+		bottomCenterStyle.font = hudFont;
+		bottomCenterStyle.normal.textColor = Color.white;
+		bottomCenterStyle.hover.textColor = Color.yellow;
+		if (GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 6 * 5 - 50, 300, 100), "MAIN MENU", bottomCenterStyle)){
+			if( loader != null ){
+				eventPublisher.publish ( new PauseEvent(true) );
+				eventPublisher.publish ( new ShowMouseEvent(true) );
+				loader.LoadLevel("start_menu");
+			} else {
+				Debug.Log ("No level loader found in scene: " + Application.loadedLevelName);
 			}
 		}
+		if ( GUI.Button (new Rect (Screen.width / 2 - 150, Screen.height / 6 * 4 - 50, 300, 100), "LEVEL SELECT", bottomCenterStyle) ){
+			if( loader != null ){
+				eventPublisher.publish ( new PauseEvent(true) );
+				eventPublisher.publish ( new ShowMouseEvent(true) );
+				loader.LoadLevel("level_select");
+			} else {
+				Debug.Log ("No level loader found in scene: " + Application.loadedLevelName);
+			}
+		}
+		GUI.Label (new Rect (0, Screen.height / 6 * 2 - 50, Screen.width, 100), "MOUSE SENSITIVITY", bottomCenterStyle);
+		Rect sliderBoxOutside = new Rect (Screen.width * 1 / 4, Screen.height * 5 / 12, Screen.width / 2, 50);
+		sens = GUI.HorizontalSlider (sliderBoxOutside, sens, 0.0F, 100.0F);
+		GUIStyle sliderStyle = GUI.skin.GetStyle("Label");
+		sliderStyle.alignment = TextAnchor.MiddleCenter;
+		sliderStyle.fontSize = 24;
+		sliderStyle.font = hudFont;
+		sliderStyle.normal.textColor = Color.white;
+		GUI.Label (new Rect (Screen.width * (sens + 50) / 200 - 30, Screen.height * 5 / 12 + 5, 60, 40),""+(int)(sens*10), sliderStyle);
+		xaxis.sensitivityX = sens;
+		yaxis.sensitivityY = sens;
 	}
 	
 	public string getCollectProgressStr( int collectRemaining, int totalCollects ){
